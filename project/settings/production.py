3741ea45410730799bf59757549ff066a00fee68
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-05-04 18:05:22 +0100

    testing bugfix

--- project/settings/production.py
+++ project/settings/production.py
@@ -3,6 +3,9 @@ import dj_database_url
 from .common import *
 
 
+INSTALLED_APPS += ['gunicorn']
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
@@ -11,8 +14,3 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # properly on Heroku.
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
