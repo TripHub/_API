a4169c8584f520ee1cfb35684f240a916fe58c63
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-05-04 17:57:40 +0100

    added dj-database-url

--- project/settings/production.py
+++ project/settings/production.py
@@ -1,4 +1,7 @@
+import dj_database_url
+
 from .common import *
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ('DJANGO_SECRET_KEY')
@@ -7,3 +10,9 @@ SECRET_KEY = os.environ('DJANGO_SECRET_KEY')
 # This ensures that Django will be able to detect a secure connection
 # properly on Heroku.
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
