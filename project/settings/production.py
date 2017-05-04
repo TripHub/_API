cf330e6365667bc1fb077d6a8a6a891cceef394e
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-05-04 17:59:23 +0100

    fixed envrion bug

--- project/settings/production.py
+++ project/settings/production.py
@@ -4,7 +4,7 @@ from .common import *
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
 # This ensures that Django will be able to detect a secure connection
