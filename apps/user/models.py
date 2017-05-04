4eb1127397accc50c0f78fd4c2ab3d8fe0993c6e
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-05-04 21:38:28 +0100

    cleaned migrations and made identifier user model's PK

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,5 +1,9 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    identifier = models.CharField(max_length=128, primary_key=True)
+
+    USERNAME_FIELD = 'identifier'
+    REQUIRED_FIELDS = ['email']
