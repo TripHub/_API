4c8918a6dbfc975e2740e192470a3281aecca337
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-06-14 22:08:51 +0100

    test member destinations

--- apps/destination/tests.py
+++ apps/destination/tests.py
@@ -27,7 +27,7 @@ class DestinationTest(APITestCase):
         response = self.client.get(reverse('destination-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_owned_trips_destinations_returned(self):
+    def test_owned_destinations_returned(self):
         """Only dest_1 should be returned."""
         dest_1 = Destination.objects.create(
             title='', trip=self.user_1_trip)
@@ -41,4 +41,16 @@ class DestinationTest(APITestCase):
         self.assertEqual(count, 1)
         self.assertEqual(len(results), 1)
         # dest_1 should be in results
+        self.assertEqual(results[0].get('id'), dest_1.uid)
+
+    def test_member_destinations_returned(self):
+        """
+        Check destinations of trips the user is a member of are returned.
+        """
+        dest_1 = Destination.objects.create(
+            title='', trip=self.user_2_trip)
+        self.user_2_trip.members.add(self.user_1)
+        self.client.force_authenticate(user=self.user_1)
+        response = self.client.get(reverse('destination-list'))
+        results = response.data.get('results')
         self.assertEqual(results[0].get('id'), dest_1.uid)
