98eb0d1deefefcebf8b9a6787062cffb3e61bf30
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-07-19 16:03:01 +0100

    added address and type to admin list

--- apps/destination/admin.py
+++ apps/destination/admin.py
@@ -4,6 +4,6 @@ from .models import Destination
 
 
 class DestinationAdmin(admin.ModelAdmin):
-    list_display = ('trip', 'uid',)
+    list_display = ('address', 'trip', 'type', 'uid',)
 
 admin.site.register(Destination, DestinationAdmin)
